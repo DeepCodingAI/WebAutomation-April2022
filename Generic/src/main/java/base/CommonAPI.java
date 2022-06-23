1640b1d230c7780506fb852a514afa7322e0efa7
mrahman <rahmanww@gmail.com>
2022-06-22 20:39:37 -0400

    cloud env browserstack

--- Generic/src/main/java/base/CommonAPI.java
+++ Generic/src/main/java/base/CommonAPI.java
@@ -3,6 +3,11 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -10,6 +15,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
@@ -19,16 +26,83 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class CommonAPI {
+
+    //cloud env access-keys
+    public String browserStackUserName = "your user name";
+    public String browserStackAccessKey = "your access key";
+    public String sauceLabsUserName = "your sace lab user name";
+    public String sauceLabsAccessKey = "your sauce lab access key";
+    //webDriver
     public static WebDriver driver = null;
 
-    @Parameters({"url"})
+    @Parameters({"useCloudEnv","envName","os","os_version","browserName","browserVersion","url"})
     @BeforeMethod
-    public void launchBrowser(String url){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public void setUp(String useCloudEnv, String envName, String OS,String os_version, String browserName,
+                      String browserVersion, String url) throws MalformedURLException {
+        if(useCloudEnv.equalsIgnoreCase("true")){
+            //get cloud driver
+            getCloudDriver(envName, OS, os_version, browserName, browserVersion);
+        }else{
+            //get local driver
+            getLocalDriver(OS,browserName);
+        }
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.navigate().to(url);
+        driver.get(url);
+        driver.manage().window().maximize();
     }
+
+    public WebDriver getLocalDriver(String OS, String browserName){
+        if(browserName.equalsIgnoreCase("chrome")){
+            if(OS.equalsIgnoreCase("OS X")){
+                //System.setProperty("webdriver.chrome.driver", "Generic/driver/chromedriver");
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }else if(OS.equalsIgnoreCase("Windows")){
+                //System.setProperty("webdriver.chrome.driver", "Generic/driver/chromedriver.exe");
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }
+        }else if(browserName.equalsIgnoreCase("firefox")){
+            if(OS.equalsIgnoreCase("OS X")){
+                //System.setProperty("webdriver.gecko.driver", "Generic/driver/geckodriver");
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }else if(OS.equalsIgnoreCase("Windows")){
+                //System.setProperty("webdriver.gecko.driver", "Generic/driver/geckodriver.exe");
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+        }else if(browserName.equalsIgnoreCase("ie")){
+            //System.setProperty("webdriver.ie.driver", "Generic/driver/internetexplorerdriver.exe");
+            WebDriverManager.iedriver().setup();
+            driver = new InternetExplorerDriver();
+
+        }else if(browserName.equalsIgnoreCase("safari")){
+            driver = new SafariDriver();
+        }
+        return driver;
+    }
+
+    public WebDriver getCloudDriver(String envName, String OS,String os_version,
+                                    String browserName, String browserVersion) throws MalformedURLException {
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("browser",browserName);
+        cap.setCapability("browserVersion",browserVersion);
+        cap.setCapability("os", OS);
+        cap.setCapability("os_version", os_version);
+        if(envName.equalsIgnoreCase("Browserstack")){
+            driver = new RemoteWebDriver(new URL("http://"+browserStackUserName+":"+browserStackAccessKey+
+                    "@hub-cloud.browserstack.com/wd/hub"),cap);
+        }else if(envName.equalsIgnoreCase("Saucelabs")) {
+            driver = new RemoteWebDriver(new URL("http://" + sauceLabsUserName + ":" + sauceLabsAccessKey +
+                    "@ondemand.saucelabs.com:80/wd/hub"), cap);
+        }
+
+        return driver;
+    }
+
 
     @AfterMethod
     public void cleanUp(){
